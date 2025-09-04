ad8f1ecb5d1f3f248e0269f7f091d592874b3197
divyathakur134 <divthakur5@gmail.com>
2025-09-04 15:59:11 +0530

    React router

---
+++ 02-Tailwindprops/src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import Card from './components/Card'
+
+function App() {
+  const [count, setCount] = useState(0)
+ 
+  return (
+    <>
+      <h1 className='bg-red-100 rounded-xl mb-5'>Tailwind test</h1>
+      <Card username="Divya" btntext="Click Me"></Card>
+    </>
+  )
+}
+
+export default App
---
+++ 05ReactRouter/src/App.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+import './App.css'
+import Header from './components/Header/Header'
+import Footer from './components/Footer/Footer'
+
+function App() {
+
+  return (
+    <>
+      
+    </>
+  )
+}
+
+export default App
